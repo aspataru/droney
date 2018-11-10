9c09ea4448fc82bf031fa83a2770e25b5c25d6af
Andrei Spataru <spataruac@gmail.com>
2018-11-10 21:21:51 +0100

    added info toggle button and fixed stopping playback on Youtube frame

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,36 +1,46 @@
 var tag = document.createElement('script');
-		tag.src = 'https://www.youtube.com/player_api';
+tag.src = 'https://www.youtube.com/player_api';
 var firstScriptTag = document.getElementsByTagName('script')[0];
-		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var tv,
-		playerDefaults = {autoplay: 0, autohide: 1, modestbranding: 0, rel: 0, showinfo: 0, controls: 0, disablekb: 1, enablejsapi: 0, iv_load_policy: 3};
+  playerDefaults = { autoplay: 0, autohide: 1, modestbranding: 0, rel: 0, showinfo: 0, controls: 0, disablekb: 1, enablejsapi: 0, iv_load_policy: 3 };
 var vid = [
-			{'videoId': 'NBIsmE1R1iA', 'startSeconds': 0, 'endSeconds': 50, 'suggestedQuality': 'hd540'}
-			// {'videoId': '9ge5PzHSS0Y', 'startSeconds': 465, 'endSeconds': 657, 'suggestedQuality': 'hd720'},
-			// {'videoId': 'OWsCt7B-KWs', 'startSeconds': 0, 'endSeconds': 240, 'suggestedQuality': 'hd720'},
-			// {'videoId': 'qMR-mPlyduE', 'startSeconds': 19, 'endSeconds': 241, 'suggestedQuality': 'hd720'}
-		],
-		randomVid = Math.floor(Math.random() * vid.length),
-    currVid = randomVid;
+  { 'videoId': 'm2Xw8Fg4Dwg', 'startSeconds': 0, 'endSeconds': 10, 'suggestedQuality': 'hd540' }
+  // {'videoId': '9ge5PzHSS0Y', 'startSeconds': 465, 'endSeconds': 657, 'suggestedQuality': 'hd720'},
+  // {'videoId': 'OWsCt7B-KWs', 'startSeconds': 0, 'endSeconds': 240, 'suggestedQuality': 'hd720'},
+  // {'videoId': 'qMR-mPlyduE', 'startSeconds': 19, 'endSeconds': 241, 'suggestedQuality': 'hd720'}
+],
+  randomVid = Math.floor(Math.random() * vid.length),
+  currVid = randomVid;
+
+var overlayShowing = false;
 
 $('.hi em:last-of-type').html(vid.length);
 
-function onYouTubePlayerAPIReady(){
-  tv = new YT.Player('tv', {events: {'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange}, playerVars: playerDefaults});
+function onYouTubePlayerAPIReady() {
+  tv = new YT.Player('tv', { events: { 'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange }, playerVars: playerDefaults });
 }
 
-function onPlayerReady(){
+function onPlayerReady() {
   tv.loadVideoById(vid[currVid]);
   tv.mute();
 }
 
 function onPlayerStateChange(e) {
-  if (e.data === 1){
+  // state is playing
+  if (e.data === 1) {
     $('#tv').addClass('active');
     $('.hi em:nth-of-type(2)').html(currVid + 1);
-  } else if (e.data === 2){
+  }
+   // state is playback finished 
+  else if (e.data === 0) {
     $('#tv').removeClass('active');
-    if(currVid === vid.length - 1){
+    tv.loadVideoById(vid[currVid]);
+    tv.seekTo(vid[currVid].startSeconds);
+  }
+  else if (e.data === 2) {
+    $('#tv').removeClass('active');
+    if (currVid === vid.length - 1) {
       currVid = 0;
     } else {
       currVid++;
@@ -40,35 +50,45 @@ function onPlayerStateChange(e) {
   }
 }
 
-function vidRescale(){
+function vidRescale() {
 
-  var w = $(window).width()+200,
-    h = $(window).height()+200;
+  var w = $(window).width() + 200,
+    h = $(window).height() + 200;
 
-  if (w/h > 16/9){
-    tv.setSize(w, w/16*9);
-    $('.tv .screen').css({'left': '0px'});
+  if (w / h > 16 / 9) {
+    tv.setSize(w, w / 16 * 9);
+    $('.tv .screen').css({ 'left': '0px' });
   } else {
-    tv.setSize(h/9*16, h);
-    $('.tv .screen').css({'left': -($('.tv .screen').outerWidth()-w)/2});
+    tv.setSize(h / 9 * 16, h);
+    $('.tv .screen').css({ 'left': -($('.tv .screen').outerWidth() - w) / 2 });
   }
 }
 
-$(window).on('load resize', function(){
+$(window).on('load resize', function () {
   vidRescale();
 });
 
-$('.hi span:first-of-type').on('click', function(){
+$('.hi span:first-of-type').on('click', function () {
   $('#tv').toggleClass('mute');
   $('.hi em:first-of-type').toggleClass('hidden');
-  if($('#tv').hasClass('mute')){
+  if ($('#tv').hasClass('mute')) {
     tv.mute();
   } else {
     tv.unMute();
   }
 });
 
-$('.hi span:last-of-type').on('click', function(){
+$('.hi span:last-of-type').on('click', function () {
   $('.hi em:nth-of-type(2)').html('~');
   tv.pauseVideo();
 });
+
+// new
+function toggleOverlay() {
+  var displayValue = "none";
+  if (overlayShowing) {
+    displayValue = "block";
+  }
+  document.getElementById("overlay").style.display = displayValue;
+  overlayShowing = !overlayShowing;
+}
